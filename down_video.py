5d2faa36d0f4280b8ea0f3fcd973f3f4191f3ae1
wgPython <33140097+wgPython@users.noreply.github.com>
2018-07-30 18:47:48 +0800

    video_spider

--- down_video.py
+++ down_video.py
@@ -20,6 +20,7 @@ from urllib.parse import urlparse
 import requests
 import time
 
+
 UA_WEB_LIST = [
     "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/22.0.1207.1 Safari/537.1",
     "Mozilla/5.0 (X11; CrOS i686 2268.111.0) AppleWebKit/536.11 (KHTML, like Gecko) Chrome/20.0.1132.57 Safari/536.11",
